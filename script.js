91e0a821245dec3a231914849b6aed931e58d9eb
Harbihorla687 <abiolaakindele293@gmail.com>
2026-05-07 18:33:58 -0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function orderProduct(productName) {
-  let phone = "234XXXXXXXXXX"; // your WhatsApp number
-  let message = `Hello, I want to order ${productName}`;
-  let url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
-
-  window.open(url, "_blank");
-}
